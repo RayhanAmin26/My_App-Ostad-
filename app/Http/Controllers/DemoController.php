6d4c2c6cfc59a0e4a6daf08722f0529e10c63140
RayhanAmin26 <rayhanamin.lm2001@gmail.com>
2024-11-28 23:04:57 +0600

    response time

---
+++ app/Http/Controllers/DemoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DemoController extends Controller
+{
+    function demo1(){
+        // return "Hello";
+        // return 100;
+        // return null;
+        // return true;
+
+        //return ['A','B','C'];
+
+        //return array(["name"=>"Rayhan","City"=>"Dhaka","Country"=>"Bangladesh"]);
+        // return response()->json(["name"=>"Rayhan","City"=>"Dhaka","Country"=>"Bangladesh"]);
+        // return response()->json(["name"=>"Rayhan","City"=>"Dhaka","Country"=>"Bangladesh"],status:203);
+
+    }
+}
